111aa3d9d198698c22f5dfe163eff8352225e386
Angela Bier <abier@corelando.com>
2015-02-02 14:44:40 -0500

    update development.rb config file

--- football/config/environments/development.rb
+++ football/config/environments/development.rb
@@ -41,4 +41,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.web_console.automount = true
+  SPORTS_DATA_API_KEY = '2fhaxup3q39q26atwekyfqej'
 end
